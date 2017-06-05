d27e95008d0d59e126ca67d0285bac6e3a8a952c
Neil Sutcliffe <neil@neilsutcliffe.com>
2017-06-05 16:33:33 +0100

    Change colour to header to match branding

--- styles/colours.js
+++ styles/colours.js
@@ -2,5 +2,7 @@ export default colours = {
   backgroundMain: '#FAFAFA',
   messageLocal: '#027BFD',
   messageRemote: '#E6E5EB',
-  headerColour: '#F3F4F6'
+  headerColour: '#F3F4F6',
+  toryBlue: '#0096DB',
+  troyBlueContrast: '#FFFFFF'
 }
